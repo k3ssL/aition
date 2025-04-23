04881c1eda792f5f221a3977e456700bee6fca5f
Aleksey <viqwer3@yandex.ru>
2025-04-24 01:57:12 +0500

    feat: create catalog page

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,7 +1,64 @@
+import classes from './CatalogPage.module.css'
+
 const CatalogPage = () => {
     return (
-        <div>
-            <p style={{color: "white"}}>Catalog page</p>
+        <div className={classes.container}>
+            <div className={classes.filters}>
+                <div className={classes.filters_item}>Телефоны</div>
+                <div className={classes.filters_item}>Ноутбуки</div>
+                <div className={classes.filters_item}>Комплектующие ПК</div>
+                <div className={classes.filters_item}>Принтеры</div>
+            </div>
+            <div className={classes.cards}>
+                <div className={classes.card_item}>
+                    <img src={"/img/ipho.png"} alt={""}/>
+                    <h3>Iphone 16</h3>
+                    <p>150.000₽</p>
+                    <button>В корзину</button>
+                </div>
+                <div className={classes.card_item}>
+                    <img src={"/img/ipho.png"} alt={""}/>
+                    <h3>Iphone 16</h3>
+                    <p>150.000₽</p>
+                    <button>В корзину</button>
+                </div>
+                <div className={classes.card_item}>
+                    <img src={"/img/ipho.png"} alt={""}/>
+                    <h3>Iphone 16</h3>
+                    <p>150.000₽</p>
+                    <button>В корзину</button>
+                </div>
+                <div className={classes.card_item}>
+                    <img src={"/img/ipho.png"} alt={""}/>
+                    <h3>Iphone 16</h3>
+                    <p>150.000₽</p>
+                    <button>В корзину</button>
+                </div>
+                <div className={classes.card_item}>
+                    <img src={"/img/ipho.png"} alt={""}/>
+                    <h3>Iphone 16</h3>
+                    <p>150.000₽</p>
+                    <button>В корзину</button>
+                </div>
+                <div className={classes.card_item}>
+                    <img src={"/img/ipho.png"} alt={""}/>
+                    <h3>Iphone 16</h3>
+                    <p>150.000₽</p>
+                    <button>В корзину</button>
+                </div>
+                <div className={classes.card_item}>
+                    <img src={"/img/ipho.png"} alt={""}/>
+                    <h3>Iphone 16</h3>
+                    <p>150.000₽</p>
+                    <button>В корзину</button>
+                </div>
+                <div className={classes.card_item}>
+                    <img src={"/img/ipho.png"} alt={""}/>
+                    <h3>Iphone 16</h3>
+                    <p>150.000₽</p>
+                    <button>В корзину</button>
+                </div>
+            </div>
         </div>
     );
 };
