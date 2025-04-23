b26dcea8aee8ddc78a6216e8604a33dc2a09ca93
Aleksey <viqwer3@yandex.ru>
2025-04-24 00:39:19 +0500

    feat: create authorization page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,14 @@
+import classes from './LoginPage.module.css'
+
 const LoginPage = () => {
     return (
-        <div>
-            <p style={{color: "white"}}>Login page</p>
+        <div className={classes.container}>
+            <form>
+                <h1>Вход</h1>
+                <input type="text" placeholder={'Логин / Эл.почта'}/>
+                <input type="text" placeholder={'Пароль'}/>
+                <button>Войти</button>
+            </form>
         </div>
     );
 };
