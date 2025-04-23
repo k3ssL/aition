10603e0815ea6daf6f0b63dc2cd43b8acc1db010
Aleksey <viqwer3@yandex.ru>
2025-04-24 00:36:57 +0500

    feat: create registration page

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,7 +1,18 @@
+import classes from './RegistrationPage.module.css'
+
 const RegistrationPage = () => {
     return (
-        <div>
-            <p style={{color: "white"}}>Registration page</p>
+        <div className={classes.container}>
+            <form>
+                <h1>Регистрация</h1>
+                <input type="text" placeholder={'Имя'}/>
+                <input type="text" placeholder={'Фамилия'}/>
+                <input type="email" placeholder={'Эл.почта'}/>
+                <input type="text" placeholder={'Телефон'}/>
+                <input type="password" placeholder={'Пароль'}/>
+                <input type="password" placeholder={'Подтверждение пароля'}/>
+                <button>Зарегистрироваться</button>
+            </form>
         </div>
     );
 };
